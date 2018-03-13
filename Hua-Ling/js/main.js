cd3eb4993e2390b99128fe07d9836e7e50d4b3a3
liaowuda <37138746+liaowuda@users.noreply.github.com>
2018-03-13 15:23:24 +0800

    shoppimg mall

---
+++ Hua-Ling/js/main.js
@@ -0,0 +1,16 @@
+  ;$(function($, window, document, undefinde){
+  	Sild = function(container, options ){
+  		"use strict" //不支持IE9的模式
+  		if(!container) return;
+
+  	}
+  });
+
+
+ //留言或发布
+ // var xx=document.getElementById("val").innerText;
+	// 	document.getElementById("msg").innerHTML=xx;
+
+
+
+
